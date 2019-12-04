21c410e756f38403c8ca97f181f28c3cdc0f4a36
Neil Prosser <neil.prosser@gmail.com>
2019-12-04 22:52:44 +0000

    Switch license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.atomist/lacinia-apollo-federation "0.1.1-SNAPSHOT"
   :description "Apollo Federation helper for Lacinia"
   :url "https://github.com/atomisthq/lacinia-apollo-federation"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License 2.0"
+            :url "https://opensource.org/licenses/Apache-2.0"}
   :dependencies [[com.walmartlabs/lacinia "0.36.0-alpha-2"]]
   :repl-options {:init-ns lacinia-apollo-federation.core}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
